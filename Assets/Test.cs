0a8ae445a129d3a924aa6deb2bc7b725931ceddb
Jacob Stelmachowske <jstelmachowske@gmail.com>
2019-11-19 15:30:39 -0500

    test commit
    test

---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Touch myTouch = Input.GetTouch(0);
+        Touch[] myTouches = Input.touches;
+        for(int i = 0; i < Input.touchCount; i++){
+            //do stuff w/ touches
+        }
+
+    }
+}
