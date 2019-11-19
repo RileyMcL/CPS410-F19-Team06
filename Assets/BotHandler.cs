82c32e5f9cba7a432f056f016283c53173c2e500
stealmyhousekey <jstelmachowske@gmail.com>
2019-11-19 18:26:46 -0500

    bot commit
    added road and cars to crash into

---
+++ Assets/BotHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotHandler : MonoBehaviour {
+    public static int movespeed = 1;
+    public Vector3 userDirection = Vector3.right;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //move
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
+
+    }
+}
