ac56b8c3397e8e232f0e66fc194e4097436b6d81
rildom <41808672+rildom@users.noreply.github.com>
2019-11-19 20:11:11 -0500

    menus
    menus

---
+++ Menus/Assets/ButtonController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+
+    public UnityEngine.UI.Image image;
+
+    int counter = 0;
+    
+
+    // Start is called before the first frame update
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    // Update is called once per frame
+    public void loadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void loadGarage()
+    {
+        SceneManager.LoadScene("Garage");
+    }
+    public void loadSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void changeColor()
+    {
+        image.color = Color.black;
+        counter++;
+        if(counter == 3)
+        {
+            image.color = Color.white;
+            counter = 0;
+        }
+        
+    }
+
+
+}
